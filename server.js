4a852b4e0c09b3ed30be149e818373ff741caca5
Faip Mardoni <faipmardoni@gmail.com>
2018-11-09 18:46:22 +0700

    change port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const next = require('next')
+require('dotenv').config()
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -40,9 +41,9 @@ app
       handle(req, res)
     })
 
-    server.listen(3000, err => {
+    server.listen(process.env.PORT, err => {
       if (err) throw err
-      console.log('> Ready on http://localhost:3000')
+      console.log(`> Ready on http://localhost:${process.env.PORT}`)
     })
   })
   .catch(error => {
